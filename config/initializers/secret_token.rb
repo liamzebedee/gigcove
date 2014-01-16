d65d950eb9fb61fedb1260824d3dab223272c0d9
User <user@romeo.(none)>
2014-01-16 20:10:13 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gigcove::Application.config.secret_key_base = '0fcdbcac3cba484d62e22537df4ed5a6459db30b84248c672f779e5d3e69dc401151fdde5d2edf8360096f811c8835597c58416bf614c63a8cb3062ebea09d29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gigcove::Application.config.session_store :cookie_store, key: '_gigcove_session'
