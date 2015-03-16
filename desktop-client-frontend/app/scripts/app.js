b34f159f3e94bbe95367c01e2b017b231eb43383
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2015-03-16 21:34:12 +1100

    more work

--- desktop-client-frontend/app/scripts/app.js
+++ desktop-client-frontend/app/scripts/app.js
@@ -39,21 +39,3 @@ angular
         });
 
     });
-/*
-if(!Modernizr.inputtypes.date) {
-  $('input[type=date]').combodate({
-    minYear: new Date().getFullYear(),
-    maxYear: new Date().getFullYear() + 1,
-    format: "DD/MM/YYYY",
-    template: "D MMM YYYY",
-    yearDescending: false,
-    smartDays: true
-  });
-}
-if(!Modernizr.inputtypes.time) {
-  $('input[type=time]').combodate({
-    format: "h:mm a",
-    template: "h : mm a",
-    minuteStep: 1
-  });
-}*/
--- desktop-client-frontend/app/scripts/controllers/App.js
+++ desktop-client-frontend/app/scripts/controllers/App.js
@@ -1,21 +1,59 @@
 angular.module('app').
-controller('AppCtrl', function($scope, uiGmapGoogleMapApi, $timeout) {
+controller('AppCtrl', function($scope, uiGmapGoogleMapApi, $timeout, $http) {
     $scope.search = {
         location: "Sydney, AU",
+        location_lat: null,
+        location_lng: null,
         cost: 25,
         under_18: false,
         fromDatePretty: "today",
         fromDate: "",
         tillDatePretty: "next week",
-        tillDate: ""
+        tillDate: "",
+        isSearchingForGigs: false
     };
 
-    $scope.$watch('search.fromDatePretty', function(newValue, oldValue){
+    $scope.search.searchForGigs = function() {
+        $scope.search.isSearchingForGigs = true;
+
+        var searchParams = {
+            cost: $scope.search.cost,
+            distance_radius: 60,
+            latitude: $scope.search.location_lat,
+            longitude: $scope.search.location_lng
+        };
+
+        $http.get('/api/gigs', { params: { q: JSON.stringify(searchParams) } }).success(function(){
+            $scope.search.gigs = response;
+        });
+    };
+
+    $scope.$watch('search.fromDatePretty', function(newValue, oldValue) {
         $scope.search.fromDate = Date.create(newValue).format(Date.ISO8601_DATETIME);
     });
-    $scope.$watch('search.tillDatePretty', function(newValue, oldValue){
+    $scope.$watch('search.tillDatePretty', function(newValue, oldValue) {
         $scope.search.tillDate = Date.create(newValue).format(Date.ISO8601_DATETIME);
     });
+    $scope.$watch('search.location', function(newValue, oldValue){
+        updateLatLngFromLocation(newValue);
+    });
+
+    function updateLatLngFromLocation(location) {
+        uiGmapGoogleMapApi.then(function(maps) {
+            geocoder = new maps.Geocoder();
+            geocoder.geocode({
+                'address': location
+            }, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    var latlng = results['geometry'].location;
+                    $scope.search.location_lat = latlng.lat();
+                    $scope.search.location_lng = latlng.lng();
+                } else {
+
+                }
+            });
+        });
+    }
 
     // get user latlng
     // geolocate to address
@@ -26,6 +64,8 @@ controller('AppCtrl', function($scope, uiGmapGoogleMapApi, $timeout) {
             browserSupportFlag = true;
             navigator.geolocation.getCurrentPosition(function(position) {
                 initialLocation = new maps.LatLng(position.coords.latitude, position.coords.longitude);
+                $scope.search.location_lat = position.coords.latitude;
+                $scope.search.location_lng = position.coords.longitude;
                 geocoder = new maps.Geocoder();
                 geocoder.geocode({
                     'latLng': initialLocation
@@ -42,7 +82,7 @@ controller('AppCtrl', function($scope, uiGmapGoogleMapApi, $timeout) {
                     }
                 });
             }, function() {
-                // fail
+                // fail to navigator.geolocation.getCurrentPosition
             });
         }
     });
